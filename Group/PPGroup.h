ce3ab954e54363ddb1cc22fb4168597108d163b9
L-vinCent <869270468@qq.com>
2018-06-04 10:28:57 +0800

    addGroup

---
+++ Group/PPGroup.h
@@ -0,0 +1,13 @@
+//
+//  PPGroup.h
+//  GCD
+//
+//  Created by Liao PanPan on 2018/6/4.
+//  Copyright © 2018年 yangyang. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface PPGroup : NSObject
+
+@end
