4ea9c46599eb2ac6cfc26cd4eb92cc645e01284f
L-vinCent <869270468@qq.com>
2018-06-19 17:47:22 +0800

    addRAC

---
+++ YZ_Four/Calsses/View/Main/Login/PPLoginViewModel.h
@@ -0,0 +1,19 @@
+//
+//  PPLoginViewModel.h
+//  YZ_Four
+//
+//  Created by Liao PanPan on 2018/6/19.
+//  Copyright © 2018年 Liao PanPan. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface PPLoginViewModel : NSObject
+
+@property (nonatomic, strong) NSString   *userName;  //手机号
+@property (nonatomic, strong) NSString   *password;   //密码
+@property (nonatomic, strong) NSString   *code;    //验证码
+
+
+
+@end
---
+++ YZ_Four/Calsses/View/Main/Login/PPLoginViewModel.m
@@ -0,0 +1,13 @@
+//
+//  PPLoginViewModel.m
+//  YZ_Four
+//
+//  Created by Liao PanPan on 2018/6/19.
+//  Copyright © 2018年 Liao PanPan. All rights reserved.
+//
+
+#import "PPLoginViewModel.h"
+
+@implementation PPLoginViewModel
+
+@end
